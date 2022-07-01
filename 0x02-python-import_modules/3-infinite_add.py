e4e3a02f125385e3307dafbb40931d42da2cfb4d
Barnabas <barnabasabuye998@gmail.com>
2022-07-01 04:18:57 +0300

    making it executable

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import sys
-    result = 0
+
+    sum_all = 0
     args = len(sys.argv)
-    for i in range(1, args):
-        result += int(sys.argv[i])
-    print("{}".format(result))
+
+    for j in range(1, args):
+        sum_all += int(sys.argv[j])
+
+    print("{:d}".format(sum_all))
