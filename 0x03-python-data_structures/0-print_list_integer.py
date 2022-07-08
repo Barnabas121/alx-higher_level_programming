c97bb76d433b3aac04d8e483d997ba353f9f34aa
Barnabas <barnabasabuye998@gmail.com>
2022-07-08 06:39:36 +0300

    deleting repo

--- 0x03-python-data_structures/0-print_list_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# File : 0-print_list_integer.py
-# Auth : 
-# Print all integers of a list
-
-
-def print_list_integer(my_list=[]):
-    for x in range(len(my_list)):
-        print("{:d}".format(my_list[x]))
